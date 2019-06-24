9262de7643846d4473afd775c4acb6b52219c3fd
Wardah <wlt_93@hotmail.com>
2019-06-24 18:43:40 +0100

    Log error if request not successful

--- scripts.js
+++ scripts.js
@@ -1,17 +1,16 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest()
 
-// Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-
-request.onload = function () {
+request.onload = function() {
   var data = JSON.parse(this.response)
 
-  data.forEach(movie => {
-    console.log(movie.title)
-  })
-
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      console.log(movie.title)
+    })
+  } else {
+    console.log('error')
+  }
 }
 
-// Send request
 request.send()
