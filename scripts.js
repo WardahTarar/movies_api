82500391485955d82be24604e5f347252788f1a3
Wardah <wlt_93@hotmail.com>
2019-06-24 18:34:09 +0100

    Display movie title

--- scripts.js
+++ scripts.js
@@ -5,8 +5,12 @@ var request = new XMLHttpRequest()
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 
 request.onload = function () {
-  // Begin accessing JSON data here
-  }
+  var data = JSON.parse(this.response)
+
+  data.forEach(movie => {
+    console.log(movie.title)
+  })
+
 }
 
 // Send request
