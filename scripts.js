521a430461e7cdfa6f9c79a4608a12f47c0f2553
Wardah <wlt_93@hotmail.com>
2019-06-25 10:48:19 +0100

    Display logo

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+const app = document.getElementById('root')
+
+const logo = document.createElement('img')
+logo.src = 'logo.png'
+
+app.appendChild(logo)
+
 var request = new XMLHttpRequest()
 
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
